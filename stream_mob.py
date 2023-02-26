9aa814b1130bbd0315a2e276037132860fffe9d0
Pavan K M <33823728+pavankm96@users.noreply.github.com>
2023-02-26 21:10:47 +0530

    Update stream_mob.py

--- stream_mob.py
+++ stream_mob.py
@@ -1,20 +1,20 @@
 import streamlit as st
-from PIL import Image
+import cv2
 import numpy as np
 
 img_file_buffer = st.camera_input("Take a picture")
 
 if img_file_buffer is not None:
-    # To read image file buffer as a PIL Image:
-    img = Image.open(img_file_buffer)
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-    # To convert PIL Image to numpy array:
-    img_array = np.array(img)
-
-    # Check the type of img_array:
+    # Check the type of cv2_img:
     # Should output: <class 'numpy.ndarray'>
-    st.write(type(img_array))
+    st.write(type(cv2_img))
 
-    # Check the shape of img_array:
+    # Check the shape of cv2_img:
     # Should output shape: (height, width, channels)
-    st.write(img_array.shape)
+    st.write(cv2_img.shape)
+    
+ st.text("opencv")
